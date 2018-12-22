64c94b537b4e4e2e40b667a2f89e6f1d687b50df
hanpifeng <997964819@qq.com>
2018-12-22 20:43:26 +0800

    6666

--- maven_day02LianXi02_parent/maven_day03_web/src/main/java/cn/itheima/controller/ItemsController.java
+++ maven_day02LianXi02_parent/maven_day03_web/src/main/java/cn/itheima/controller/ItemsController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//万年涛的爸爸
 @Controller
 @RequestMapping("/items")
 public class ItemsController {
@@ -17,5 +17,6 @@ public class ItemsController {
         Items items = is.findById(1);
         model.addAttribute("item",items);
         return "itemDetail";
+
     }
 }
