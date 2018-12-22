e1376c13e992ae089731da863e0d1d32051d4f56
suzhonghua <suzhonghua@163.com>
2018-12-22 20:21:02 +0800

    修改dao保存items

--- maven_day02LianXi02_parent/maven_day02_dao/src/main/java/cn/itheima/dao/ItemsMapper.java
+++ maven_day02LianXi02_parent/maven_day02_dao/src/main/java/cn/itheima/dao/ItemsMapper.java
@@ -1,7 +1,29 @@
 package cn.itheima.dao;
 
 import cn.itheima.pojo.Items;
+import com.sun.scenario.effect.impl.state.LinearConvolveRenderState;
+
+import java.util.List;
 
 public interface ItemsMapper {
+
+    /**
+     * 根据id查找
+     * @param id
+     * @return
+     */
     public Items findById(Integer id);
+
+    /**
+     * 查询所有items
+     * @return
+     */
+    List<Items> findAll();
+
+    /**
+     * 保存items
+     * @param items
+     */
+    void save(Items items);
+
 }
